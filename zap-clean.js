138137be201b8e1f86daa4145cd78025b9e77cbd
Ryan Lee <ryan.lee@zaplabs.com>
2018-02-08 11:21:28 -0800

    clear out webapps folder

--- zap-clean.js
+++ zap-clean.js
@@ -5,5 +5,7 @@ exeq(
   `cd ${env.ZAP}`,
   `${env.JAVA_HOME}/bin/java -Dmaven.multiModuleProjectDirectory=${env.ZAP} -Dmaven.home=${env.M2_HOME} -Dclassworlds.conf=${env.M2_HOME}/bin/m2.conf -Dfile.encoding=UTF-8 -classpath ${env.M2_HOME}/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -DskipTests=true clean -U`,
   `cd ${env.BOARDWALK}`,
-  `${JAVA_HOME}/bin/java -Dmaven.multiModuleProjectDirectory=${BOARDWALK} -Dmaven.home=${M2_HOME} -Dclassworlds.conf=${M2_HOME}/bin/m2.conf -Dfile.encoding=UTF-8 -classpath ${M2_HOME}/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -DskipTests=true clean -U`
+  `${JAVA_HOME}/bin/java -Dmaven.multiModuleProjectDirectory=${BOARDWALK} -Dmaven.home=${M2_HOME} -Dclassworlds.conf=${M2_HOME}/bin/m2.conf -Dfile.encoding=UTF-8 -classpath ${M2_HOME}/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -DskipTests=true clean -U`,
+  `rm -rf ${env.CATALINA_HOME}/webapps/ROOT`,
+  `rm -rf ${env.CATALINA_HOME}/webapps/ROOT.war`
 )
