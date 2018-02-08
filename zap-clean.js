a81392db19daabe2dd32b0d64efad4e0cbd26c5f
Ryan Lee <ryan.lee@zaplabs.com>
2018-02-08 14:55:54 -0800

    i am not a smart man

--- zap-clean.js
+++ zap-clean.js
@@ -2,11 +2,11 @@ const exeq = require('exeq');
 const env = require('./config');
 
 exeq(
-  `echo Cleaning boardwalk and zapv3...`
+  `echo Cleaning boardwalk and zapv3...`,
   `cd ${env.ZAP}`,
   `${env.JAVA_HOME}/bin/java -Dmaven.multiModuleProjectDirectory=${env.ZAP} -Dmaven.home=${env.M2_HOME} -Dclassworlds.conf=${env.M2_HOME}/bin/m2.conf -Dfile.encoding=UTF-8 -classpath ${env.M2_HOME}/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -DskipTests=true clean -U`,
   `cd ${env.BOARDWALK}`,
-  `${JAVA_HOME}/bin/java -Dmaven.multiModuleProjectDirectory=${BOARDWALK} -Dmaven.home=${M2_HOME} -Dclassworlds.conf=${M2_HOME}/bin/m2.conf -Dfile.encoding=UTF-8 -classpath ${M2_HOME}/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -DskipTests=true clean -U`,
+  `${env.JAVA_HOME}/bin/java -Dmaven.multiModuleProjectDirectory=${env.BOARDWALK} -Dmaven.home=${env.M2_HOME} -Dclassworlds.conf=${env.M2_HOME}/bin/m2.conf -Dfile.encoding=UTF-8 -classpath ${env.M2_HOME}/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -DskipTests=true clean -U`,
   `rm -rf ${env.CATALINA_HOME}/webapps/ROOT`,
   `rm -rf ${env.CATALINA_HOME}/webapps/ROOT.war`
 )
