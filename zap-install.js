cdce9641bc2203eb94ab9ad8dc771ce9ea20eba8
Ryan Lee <ryan.lee@zaplabs.com>
2018-02-08 14:50:11 -0800

    typo

--- zap-install.js
+++ zap-install.js
@@ -5,5 +5,5 @@ exeq(
   `rm -rf ${env.CATALINA_HOME}/webapps/ROOT`,
   `rm -rf ${env.CATALINA_HOME}/webapps/ROOT.war`,
   `cd ${env.BOARDWALK} && ${env.JAVA_HOME}/bin/java -Dmaven.multiModuleProjectDirectory=${env.BOARDWALK} -Dmaven.home=${env.M2_HOME} -Dclassworlds.conf=${env.M2_HOME}/bin/m2.conf -Dfile.encoding=UTF-8 -classpath ${env.M2_HOME}/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -DskipTests=true install -P maestro-aur`,
-  `cd ${env.ZAP} && ${env.JAVA_HOME}/bin/java -Dmaven.multiModuleProjectDirectory=${env.ZAP} -Dmaven.home=${env.M2_HOME} -Dclassworlds.conf=${env.M2_HOME}/bin/m2.conf -Dfile.encoding=UTF-8 -classpath ${env.M2_HOME}/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -DskipTests=true install -P maestro-aur && cp /Users/$USER/.m2/repository/com/ziprealty/zapp/DEVELOP-SNAPSHOT/zapp-DEVELOP-SNAPSHOT.war ${env.CATALINA_HOME}/webapps/ROOT.war`,
+  `cd ${env.ZAP} && ${env.JAVA_HOME}/bin/java -Dmaven.multiModuleProjectDirectory=${env.ZAP} -Dmaven.home=${env.M2_HOME} -Dclassworlds.conf=${env.M2_HOME}/bin/m2.conf -Dfile.encoding=UTF-8 -classpath ${env.M2_HOME}/boot/plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -DskipTests=true install -P maestro-aur && cp /Users/$USER/.m2/repository/com/ziprealty/zapp/DEVELOP-SNAPSHOT/zapp-DEVELOP-SNAPSHOT.war ${env.CATALINA_HOME}/webapps/ROOT.war`
 );
